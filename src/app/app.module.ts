ac6a1584f2967e00b8a9df087cbd8f553a341c7f
Hattori21 <jaume.montalvo@gmail.com>
2018-04-19 12:07:45 +0200

    Components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { MembersComponent } from './components/members/members.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SignupComponent,
+    LoginComponent,
+    MembersComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,

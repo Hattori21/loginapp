dc3789496f11017295cb7663388f45552c81ac96
Hattori21 <jaume.montalvo@gmail.com>
2018-04-20 04:03:11 +0200

    Protected urls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,13 @@ import { SignupComponent } from './components/signup/signup.component';
 import { MembersComponent } from './components/members/members.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
+import { AuthGuard } from './guards/auth.guard';
+
 const routes: Routes = [
 	{path: '', component: HomeComponent},
 	{path: 'login', component: LoginComponent},
 	{path: 'signup', component: SignupComponent},
-	{path: 'members', component: MembersComponent},
+	{path: 'members', component: MembersComponent, canActivate: [AuthGuard]},
 	{path: '**', component: NotFoundComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 
 import { AuthService } from './services/auth.service';
-
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   declarations: [
@@ -37,7 +37,10 @@ import { AuthService } from './services/auth.service';
     AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebaseConfig, 'loginapp')
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
